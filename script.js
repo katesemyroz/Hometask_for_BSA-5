cf2f4cfae6b2915ba8bbfc7abccdc26e4f34a04d
Kateryna Semyroz <kateryna.semyroz@gmail.com>
2017-07-11 01:04:40 +0200

    Added transition animation and fixed several bugs

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $( "#cross_button" ).hide();
 $( "#hamburger_menu" ).hide();
 $( "#hamburger_button" ).click(function() {
-$( "#hamburger_menu" ).slideToggle( "slow", function() {
-$( "#hamburger_button" ).hide();
-$( "#cross_button" ).show();
-});
+	$( "#hamburger_menu" ).slideToggle( "slow", function() {
+		$( "#hamburger_button" ).hide();
+		$( "#cross_button" ).show();
+	});
 });
 
 
 $( "#cross_button" ).click(function() {
-$( "#hamburger_menu" ).slideToggle( "slow", function() {
-$( "#cross_button" ).hide();
-$( "#hamburger_button" ).show();
-});
+	$( "#hamburger_menu" ).slideToggle( "slow", function() {
+		$( "#cross_button" ).hide();
+		$( "#hamburger_button" ).show();
+	});
 });
